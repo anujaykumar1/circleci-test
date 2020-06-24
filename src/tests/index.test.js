0bb16752b9ac38f3fe26cbfee2d9b904da0c30b1
= <=>
2020-06-24 13:47:13 +0530

    failed test

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -3,7 +3,7 @@ const {add, sub ,product,divide} = require('../index');
 test("Math test suite",()=>{
 
     expect(add(1,0)).toBe(1)
-    expect(sub(1,1)).toBe(0);
+    expect(sub(1,1)).toBe(1);
     expect(product(1,1)).toBe(1)
     expect(divide(1,1)).toBe(1)
 })
